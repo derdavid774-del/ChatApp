4dc4d5dcf72feab3eac728c4789636854752cec5
David <derdavid774@gmail.com>
2025-12-14 21:37:06 +0100

    A5-26

--- login.php
+++ login.php
@@ -27,36 +27,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
-    <div class="container">
-        <img src="images/chat.png" alt="Chat App Logo">
-
-        <h1>Please sign in</h1>
-        <form id="signIn" action="login.php" method="POST">
-            <fieldset class="data">
-                <legend>Login</legend>
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" placeholder="Username" required>
-
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Password" required>
-            </fieldset>
-        </form>
+<body class="bg-light vh-100 d-flex align-items-center justify-content-center">
+    <div style="max-width: 350px;">
+        <div class="text-center mb-5">
+            <img src="images/chat.png" alt="Chat App Logo" class="rounded-circle" width="175">
+        </div>
+        <div class="card">
+            <div class="card-body p-5">
+                <h2 class="h4 mb-4 text-center">Please sign in</h2>
+                <form id="signIn" action="login.php" method="POST">
+                    <div class="mb-3">
+                        <input type="text" class="form-control form-control-lg" id="username" name="username" placeholder="Username" required>
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control form-control-lg" id="password" name="password" placeholder="Password" required>
+                    </div>
+                </form>
+        
+                <div class="btn-group w-100">
+                    <form action="register.php" method="get" class="flex-grow-1">
+                        <button class="btn btn-secondary btn-lg w-100">Register</button>
+                    </form>
+                    <button class="btn btn-primary btn-lg" form="signIn">Login</button>
+                </div>
+            </div>
+        </div> 
 
         <?php if (isset($error)) { ?>
                 <div class="error"> <?= $error ?> </div>
         <?php } ?>
 
-        <div class="container-btn">
-            <form action="register.php" method="get">
-                <button>Register</button>
-            </form>
-            <button class="btn-blue" form="signIn">Login</button>
-        </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- logout.php
+++ logout.php
@@ -9,16 +9,24 @@ session_unset();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logout</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 
-<body>
-    <div class="container">
-        <img src="images/logout.png" alt="Chat App Logo">
-        <h1>Logged out...</h1>
-        <p>See u!</p>
-        <a href="login.php">Login again</a>
+<body class="bg-light vh-100 d-flex align-items-center justify-content-center">
+    <div style="width: 350px;">
+        <div class="text-center mb-5">
+            <img src="images/logout.png" alt="Chat App Logo" class="rounded-circle" width="175">
+        </div>
+        <div class="card">
+            <div class="card-body p-5">
+                <h1 class="h4 mb-3 text-center">Logged out...</h1>
+                <p class="text-center">See u!</p>
+                <a href="login.php" class="btn btn-secondary w-100">Login again</a>
+            </div>
+        </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
